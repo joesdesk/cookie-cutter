c2827e34f0eec1694b634c1e358f1d774e2feef8
Jomar Anthony Sastrillo <sastrill@alumni.ubc.ca>
2017-11-19 12:52:32 -0800

    Added script

---
+++ cookie-cutter.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Attempt to create project directory
+mkdir $1 || exit 1
+
+# If successful, proceed to create project folder structure
+cd $1
+mkdir data results src
+
+# Create README
+touch README.md
+echo "#project-name" >> README.md
+echo "" >> README.md
+
+# Create empty Rmarkdown file
+cd src
+touch untitled.Rmd
+echo "---
+output:
+  github_document
+---
+
+# " >> untitled.Rmd
+
+# Exit with success
+exit 0
