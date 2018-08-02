134cf454de34710e82f8433b45335edb7388d34a
joesdesk <jomar.sastrillo@gmail.com>
2018-08-02 07:11:33 -0700

    Ability to install package

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
 from setuptools import setup
 
+
 setup(name='pytemplate',
       version='0.1',
       description='A template for python packages',
       url='https://github.com/joesdesk/pytemplate',
       author='Jomar Sastrillo',
+      author_email='jomar.sastrillo@gmail.com',
       packages=['pytemplate'],
       install_requires=['pytest',
-                        'setuptools'],
-      zip_safe=False)
+                        'setuptools']
+)
